457f5bdbcd86d85a985f6a626420b752e7167d54
Eric Kelly <me@useric.com>
2013-09-19 21:00:08 -0400

    Changed to create and close sockets with each request

--- client.py
+++ client.py
@@ -79,6 +79,11 @@ def open_socket(server, port):
     s.connect((server, port))
     return s
 
+# Create a connection to the server
+# -> Socket
+def create_connection():
+    return open_socket(FAKEBOOK_HOST, 80)
+
 # Close the socket
 def close_socket(socket):
     socket.close()
@@ -108,34 +113,11 @@ def parse_header(data):
             header['response_code'] = line[0].split(' ')[1]
     return header
 
-# Create a GET request for the specified url path
-def http_get(socket, host, path, cookies=""):
-    header =  "GET %s HTTP/1.1\n" % path
-    header += "Host: %s\n" % host
-    if cookies != "":
-        header += "Cookie: %s\n" % cookies 
-    header += "\n"
-
-    sent_bytes = send_data(socket, header)
-
-    response = split_http(recv_data(socket))
-    header = parse_header(response[0])
-    body = response[1]
-
-    return (header, body)
-
-# Convert map into form encoded key=value string
-def form_encode(form_data):
-    ret = ""
-    for key,value in form_data.items():
-        if len(ret):
-            ret += "&"
-        ret += "%s=%s" % (key.replace(" ", "+"),value.replace(" ", "+"))
-    return ret
-
-# Socket String String Dict String -> HTTPResponse
+# String String Dict String -> HTTPResponse
 # Send a POST for the specified url path with the specified form data
-def http_post(socket, host, path, form_data, cookies=""):
+def http_post(host, path, form_data, cookies):
+    socket = create_connection()
+
     encoded = form_encode(form_data)
     header = "POST %s HTTP/1.1\n" % path
     header += "Host: %s\n" % host
@@ -151,25 +133,53 @@ def http_post(socket, host, path, form_data, cookies=""):
     header = parse_header(response[0])
     body = response[1]
 
+    close_connection(socket)
+
     return (header, body)
 
-# Socket -> [String -> HTTPResponse]
-# Wrap the socket to allow for easy sending
-def wrap_get(socket):
-    return lambda path: http_get(socket, FAKEBOOK_HOST, path, retrieve_cookies())
+# Create a GET request for the specified url path
+def http_get(host, path, cookies):
+    socket = create_connection()
+    header =  "GET %s HTTP/1.1\n" % path
+    header += "Host: %s\n" % host
+    if cookies != None:
+        header += "Cookie: %s\n" % cookies 
+    header += "\n"
 
-# Socket -> [String Dict -> HTTPResponse]
-# Wrap the socket to allow for easy sending
-def wrap_post(socket):
-    return lambda path, form_data: http_post(socket, FAKEBOOK_HOST, path, form_data, retrieve_cookies())
+    sent_bytes = send_data(socket, header)
+
+    response = split_http(recv_data(socket))
+    header = parse_header(response[0])
+    body = response[1]
+
+    close_connection(socket)
+
+    return (header, body)
+
+# Convert map into form encoded key=value string
+def form_encode(form_data):
+    ret = ""
+    for key,value in form_data.items():
+        if len(ret):
+            ret += "&"
+        ret += "%s=%s" % (key.replace(" ", "+"),value.replace(" ", "+"))
+    return ret
+
+# String -> HTTPResponse
+# Get a path from the server
+def get(path):
+    return http_get(FAKEBOOK_HOST, path, retrieve_cookies())
+
+# String Dict -> HTTPResponse
+# Get a path from the server
+def post(path, form_data):
+    return http_post(FAKEBOOK_HOST, path, form_data, retrieve_cookies())
     
 # Login
 # Socket ->
 def do_login(socket, username, password):
     global to_visit
     global cookie_store
-    get = wrap_get(socket)
-    post = wrap_post(socket)
     (header, login_page) = get(FAKEBOOK_HOME)
     store_cookies(header)
     token = cookie_store["csrftoken"]
@@ -187,10 +197,6 @@ def do_login(socket, username, password):
 # Entry point to the program
 def main():
     args = parse_input()
-
-    # Create a connection to the server
-    socket = open_socket(FAKEBOOK_HOST, 80)
-    get = wrap_get(socket)    
 
     if not do_login(socket, args.username, args.password):
        print "Could not login" 
